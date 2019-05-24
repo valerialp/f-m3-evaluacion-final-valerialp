60fc036f26331034662495b060b77668f1b104cf
valerialp <valeria.lorentep@gmail.com>
2019-05-24 15:37:01 +0200

    Create Home component estructure

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import { Route, Switch } from 'react-router-dom';
+import Home from '../Home'
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      
+    }
   }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          
+          hola
         </header>
         <Switch>
 		      <Route exact path="/" component={Home} />
-		      <Route path="/about" component={Filters} />
+		      {/* <Route path="/about" component={Filters} /> */}
         </Switch>
       </div>
     );
---
+++ src/components/Home/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Home extends React.Component {
+
+    render() {
+        return(
+        <p>hola de home</p>)
+    }
+}
+export default Home;
