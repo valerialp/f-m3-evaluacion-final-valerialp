055ffc18c7b87cac56417da75a4140439770fcf8
valerialp <valeria.lorentep@gmail.com>
2019-05-26 17:00:49 +0200

    Add styles

--- src/components/Filters/index.js
+++ src/components/Filters/index.js
@@ -1,23 +1,57 @@
-import React, { Component, Fragment}  from 'react';
-import './styles.scss';
+import React, { Component } from "react";
+import "./styles.scss";
+import Houses from "../Houses";
 
 class Filters extends Component {
-    render(){
-        return(
-            <Fragment>
-                <label htmlFor="search" className="title-seeker">Búsqueda</label>
-                <input 
-                    className="seeker"
-                    type="text" 
-                    name="seeker" 
-                    id="seeker" 
-                    value={this.props.nameValue} 
-                    placeholder="Harry Potter"
-                    onChange={this.props.onChangeSearch}
-                />
-            </Fragment>
-        )
+  constructor(props){
+    super(props)
+    this.state = {
+      open: 'hidden',
     }
+  }
+
+  
+  render() {
+    const {
+      nameValue,
+      onChangeSearch,
+      onClickHouse,
+      houses
+    } = this.props;
+    return (
+      <form className="main-form">
+          <div className="seeker-form">
+            <button type="button" className="btn btn-seeker">
+              <i className="fas fa-search"> </i>
+            </button>
+            <section className="title-seeker">
+              <label htmlFor="search" className="sub__title-seeker">
+                Búsqueda
+              </label>
+              <input
+                className="seeker"
+                type="text"
+                name="seeker"
+                id="seeker"
+                value={nameValue}
+                placeholder="Harry Potter"
+                onChange={onChangeSearch}
+              />
+            </section>
+          </div>
+          <div className="house-form">
+            <button type="button" className="btn btn-house">
+              <i className="fas fa-bolt"> </i>
+            </button>
+            <section className="section-houses">
+              <Houses onClickHouse={onClickHouse} houses={houses} />
+            </section>
+          </div>
+          <div className="theme-form">
+          </div>
+      </form>
+    );
+  }
 }
 
 export default Filters;
